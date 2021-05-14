ec408bfaf2f57e77b02446261a4f1551e79f2601
bgoonz <bryan.guner@gmail.com>
2021-05-14 16:51:55 -0400

    May 14, 2021 4:51 PM

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,34 @@
-// Write your Character component here
+import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components'
+// import { makeStyles } from '@material-ui/core/styles';
+// import ListSubheader from '@material-ui/core/ListSubheader';
+// import List from '@material-ui/core/List';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import Collapse from '@material-ui/core/Collapse';
+// import ExpandLess from '@material-ui/icons/ExpandLess';
+// import ExpandMore from '@material-ui/icons/ExpandMore';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Character;
