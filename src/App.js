977d26f131e53f5701e0d549af6ad4513371cb1b
bgoonz <bryan.guner@gmail.com>
2021-05-14 16:57:07 -0400

    May 14, 2021 4:57 PM

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+// import React from 'react';
+// import './App.css';
+// 
+// const App = () => {
+//   // Try to think through what state you'll need for this app before starting. Then build out
+//   // the state properties here.
+// 
+//   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+//   // side effect in a component, you want to think about which state and/or props it should
+//   // sync up with, if any.
+// 
+//   return (
+//     <div className="App">
+//       <h1 className="Header">Characters</h1>
+//     </div>
+//   );
+// }
+// 
+// export default App;
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import Character from './components/Character'
+import Character from './components/Character.js'
 
-
-
+const url = "https://swapi.dev/api/people";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  const [ characters, setCharacters ] = useState( [] );
+  
+  
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
