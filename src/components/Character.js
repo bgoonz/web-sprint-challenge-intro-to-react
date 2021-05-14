4da432630d9385188cf40a6176ba2bb862213cd7
bgoonz <bryan.guner@gmail.com>
2021-05-14 17:12:05 -0400

    May 14, 2021 5:12 PM

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,6 @@
 // Write your Character component here
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
